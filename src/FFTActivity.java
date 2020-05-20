8899355fcd47359dc6893a231335651bc316258f
battl <battlefieldheroes43@gmail.com>
2020-05-20 19:58:09 +0300

    Added writing to CVS file

--- src/FFTActivity.java
+++ src/FFTActivity.java
@@ -62,165 +62,44 @@ public class FFTActivity extends JFrame{
 
 
     public double[] FFTQ(String fileLoc) {
-
-        final String TAG = "";
-        FastFourierTransformer fft;
-        long myDataSize;
-        Complex fftArray;
-        String myPath;
-        long myChunkSize;
-        long mySubChunk1Size;
-        int myFormat;
-        long myChannels;
-        long mySampleRate;
-        long myByteRate;
-        int myBlockAlign;
-        int myBitsPerSample;
-        double[] arrayR = new double[2048];
-        File fileIn;
-        //AudioInputStream audioInputStream;
-        ApacheFFT apacheFFT = new ApacheFFT();
-        fileIn = new File(fileLoc);
-        DataInputStream inFile = null;
-        byte[] myData;
-        // private long myChunkSize;
-
-        WindowFunction w = new HanningWindow();
-
-        byte[] tmpLong = new byte[4];
-        byte[] tmpInt = new byte[2];
+        double[] arrayR = new double[4096];
         try {
-            inFile = new DataInputStream(new FileInputStream(fileIn));
 
 
-            String chunkID = "" + (char) inFile.readByte() + (char) inFile.readByte() + (char) inFile.readByte() + (char) inFile.readByte();
+            ApacheFFT apacheFFT = new ApacheFFT();
+            WindowFunction w = new RectangularWindow();
 
 
-            inFile.read(tmpLong); // read the ChunkSize
-            myChunkSize = byteArrayToLong(tmpLong);
-
-            String format = "" + (char) inFile.readByte() + (char) inFile.readByte() + (char) inFile.readByte() + (char) inFile.readByte();
-
-            // print what we've read so far
-            //System.out.println("chunkID:" + chunkID + " chunk1Size:" + myChunkSize + " format:" + format); // for debugging only
-            //System.out.println("==========================================");
-
-            //System.out.println("chunkID:" + chunkID + " chunk1Size:" + myChunkSize + " format:" + format);
+            WavIO wave = readWavBytes(fileLoc);
+            short[] b = new short[wave.myData.length / 2];
+            for (int i = 0; i < wave.myData.length - 1; i += 2)
+                b[i / 2] = twoBytesToShort(wave.myData[i + 1], wave.myData[i]);
 
 
-            String subChunk1ID = "" + (char) inFile.readByte() + (char) inFile.readByte() + (char) inFile.readByte() + (char) inFile.readByte();
+            double[] dataNew = new double[b.length];
 
-            inFile.read(tmpLong); // read the SubChunk1Size
-            mySubChunk1Size = byteArrayToLong(tmpLong);
-
-            //System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAASTA este SUBCHUNKSIZE");
-            inFile.read(tmpInt); // read the audio format.  This should be 1 for PCM
-            myFormat = byteArrayToInt(tmpInt);
-
-            inFile.read(tmpInt); // read the # of channels (1 or 2)
-            myChannels = byteArrayToInt(tmpInt);
-
-            inFile.read(tmpLong); // read the samplerate
-            mySampleRate = byteArrayToLong(tmpLong);
-
-            inFile.read(tmpLong); // read the byterate
-            myByteRate = byteArrayToLong(tmpLong);
-
-            inFile.read(tmpInt); // read the blockalign
-            myBlockAlign = byteArrayToInt(tmpInt);
-
-            inFile.read(tmpInt); // read the bitspersample
-            myBitsPerSample = byteArrayToInt(tmpInt);
-
-
-            // print what we've read so far
-            //System.out.println("SubChunk1ID:" + subChunk1ID + " SubChunk1Size:" + mySubChunk1Size + " AudioFormat:" + myFormat + " Channels:" + myChannels + " SampleRate:" + mySampleRate);
-
-
-            // read the data chunk header - reading this IS necessary, because not all wav files will have the data chunk here - for now, we're just assuming that the data chunk is here
-            String dataChunkID = "" + (char) inFile.readByte() + (char) inFile.readByte() + (char) inFile.readByte() + (char) inFile.readByte();
-
-            inFile.read(tmpLong); // read the size of the data
-            myDataSize = byteArrayToLong(tmpLong);
-
-
-            //System.out.println("MYYYYYY DAAAAAAAAAAATAAAAAA SIIIIIIZE ISSSSSS   " + myDataSize);
-            // read the data chunk
-            myData = new byte[(int) myDataSize];
-            //inFile.read(myData);
-
-
-            double[] dataNew = new double[(int) myDataSize / 2];
-            myData = new byte[(int) myDataSize];
-            for (int i = 0; i < myDataSize / 2; i++) {
-                short val = (short) ((inFile.readByte() & 0xFF) | (inFile.readByte() & 0xFF) << 8);
-                dataNew[i] = (double) val;
+            for (int i = 0; i < dataNew.length; i++) {
+                dataNew[i] = b[i];
             }
-        /*for (int j = 0; j < dataNew.length; j++) {
-            System.out.println("MY DATA IS HERE" + dataNew[j]);
-        }*/
-
-            //System.out.println("ASTA E DIMENSIUNEA DATELOR " + myDataSize);
-            //System.out.println("ASTA E DIMENSIUNEA LA DATA PRELEVATA" + dataNew.length);
-
-            double valMax = 0;
-            int indexMax = 0;
-
-            int indexInterest = 0;
-
-            for (int q = 0; q < dataNew.length; q++) {
-                if (dataNew[q] > valMax) {
-                    valMax = dataNew[q];
-                    indexMax = q;
-                }
-
-            }
-
-            double[] arrayInterest = new double[dataNew.length - indexMax];
-
-            while (dataNew[indexMax] > valMax * 0.1) {
-                arrayInterest[indexInterest] = dataNew[indexMax];
-                indexMax++;
-                indexInterest++;
-            }
-
 
             apacheFFT.forward(dataNew, (float) 44100, w);
+
             Spectrum s = apacheFFT.getMagnitudeSpectrum();
             double[] freq = s.array();
-            double[] array = new double[freq.length];
+
             for (int i = 0; i < freq.length; i++) {
-                array[i] = freq[i];
-                //if(array[i]<0)
-                //array[i]=0;
+                freq[i] = 10 * Math.log10(10 * Math.abs(freq[i]));
             }
 
-            //for (int i = 0; i < array.length; i++)
-            //    System.out.println("ASTEA SUNT FRECVENTELE" + Math.abs(array[i]));
-
-            //System.out.println("MARIMEA LA FRECVENTE" + freq.length);
-
-            //getSampleRate(mySampleRate);
-
-
-            //arrayR[0]=chunkID;
-            //arrayR[1]=format;
-            //arrayR[2]=subChunk1ID;
-            //arrayR[3]=Long.toString(mySubChunk1Size);
-            //arrayR[4]=Long.toString(myChannels);
-            //arrayR[5]=Long.toString(mySampleRate);
-            for (int ind = 0; ind < arrayR.length; ind++) {
-                arrayR[ind] = array[ind]*100;
+            for(int ind=0;ind<arrayR.length;ind++)
+            {
+                arrayR[ind]=freq[ind];
             }
-
-            //System.out.println("AIIIIIIIIIIIIIIIIIIICIIIIIIIIIIII SE AFLA ============================== CHUNK ID");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return arrayR;
-        //for(int ind=0;ind<arrayR.length;ind++)
-        //System.out.println(arrayR[ind]);
 
+        return arrayR;
     }
     public static void main(String[] args)
     {
@@ -262,9 +141,15 @@ public class FFTActivity extends JFrame{
         writer.close();
     }
 
+    public static WavIO readWavBytes(String path){
+        WavIO wave = new WavIO(path);
+        wave.read();
+        return wave;
+    }
 
-
-
+    static short twoBytesToShort(byte b1, byte b2) {
+        return (short) ((b1 << 8) | (b2 & 0xFF));
+    }
 
 
     public static int byteArrayToInt(byte[] b)
@@ -275,27 +160,6 @@ public class FFTActivity extends JFrame{
         return (int)( high << 8 | low );
     }
 
-    public static long byteArrayToLong(byte[] b)
-    {
-        int start = 0;
-        int i = 0;
-        int len = 4;
-        int cnt = 0;
-        byte[] tmp = new byte[len];
-        for (i = start; i < (start + len); i++)
-        {
-            tmp[cnt] = b[i];
-            cnt++;
-        }
-        long accum = 0;
-        i = 0;
-        for ( int shiftBy = 0; shiftBy < 32; shiftBy += 8 )
-        {
-            accum |= ( (long)( tmp[i] & 0xff ) ) << shiftBy;
-            i++;
-        }
-        return accum;
-    }
 
 }
 
